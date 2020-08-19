543bfbff9bf664e0d2cc68e9d133b90ea72029b0
AnaRadakk <45067209+AnaRadakk@users.noreply.github.com>
2020-08-19 11:39:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $("select").change(function(){
+    if ($(this).val()=="") $(this).css({color: "#aaa"});
+    else $(this).css({color: "#000"});
+  });
+
+});
+$('carousel-item Questionare2').show(function(){
+$(duble).addClass("active");
+});
